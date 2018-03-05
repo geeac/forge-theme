678b982704efb47ff8ee561af40f749c25fac6f9
geeac <eugenia.cosinschi@gmail.com>
2018-03-05 14:01:11 +0200

    first commit

---
+++ functions.php
@@ -0,0 +1,277 @@
+<?php
+/**
+ * Genesis Sample.
+ *
+ * This file adds functions to the Genesis Sample Theme.
+ *
+ * @package Genesis Sample
+ * @author  StudioPress
+ * @license GPL-2.0+
+ * @link    http://www.studiopress.com/
+ */
+
+// Start the engine.
+include_once( get_template_directory() . '/lib/init.php' );
+
+// Setup Theme.
+include_once( get_stylesheet_directory() . '/lib/theme-defaults.php' );
+
+// Set Localization (do not remove).
+add_action( 'after_setup_theme', 'genesis_sample_localization_setup' );
+function genesis_sample_localization_setup(){
+	load_child_theme_textdomain( 'genesis-sample', get_stylesheet_directory() . '/languages' );
+}
+
+// Add the helper functions.
+include_once( get_stylesheet_directory() . '/lib/helper-functions.php' );
+
+// Add Image upload and Color select to WordPress Theme Customizer.
+require_once( get_stylesheet_directory() . '/lib/customize.php' );
+
+// Include Customizer CSS.
+include_once( get_stylesheet_directory() . '/lib/output.php' );
+
+// Add WooCommerce support.
+include_once( get_stylesheet_directory() . '/lib/woocommerce/woocommerce-setup.php' );
+
+// Add the required WooCommerce styles and Customizer CSS.
+include_once( get_stylesheet_directory() . '/lib/woocommerce/woocommerce-output.php' );
+
+// Add the Genesis Connect WooCommerce notice.
+include_once( get_stylesheet_directory() . '/lib/woocommerce/woocommerce-notice.php' );
+
+// Child theme (do not remove).
+define( 'CHILD_THEME_NAME', 'Forge Theme' );
+define( 'CHILD_THEME_URL', 'http://www.studiopress.com/' );
+define( 'CHILD_THEME_VERSION', '1.0.0' );
+
+// Enqueue Scripts and Styles.
+add_action( 'wp_enqueue_scripts', 'genesis_sample_enqueue_scripts_styles' );
+function genesis_sample_enqueue_scripts_styles() {
+
+	wp_enqueue_style( 'genesis-sample-fonts', '//fonts.googleapis.com/css?family=Bitter:400,400i,700|Open+Sans:400,400i,600,600i,700,700i', array(), CHILD_THEME_VERSION );
+	wp_enqueue_style( 'dashicons' );
+
+
+	$suffix = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min';
+	//wp_enqueue_script( 'genesis-sample-responsive-menu', get_stylesheet_directory_uri() . "/js/responsive-menus{$suffix}.js", array( 'jquery' ), CHILD_THEME_VERSION, true );
+	wp_enqueue_script( 'genesis-sample-responsive-menu', get_stylesheet_directory_uri() . "/js/responsive-menus.js", array( 'jquery' ), CHILD_THEME_VERSION, true );
+
+	wp_localize_script(
+		'genesis-sample-responsive-menu',
+		'genesis_responsive_menu',
+		genesis_sample_responsive_menu_settings()
+	);
+
+}
+
+// Define our responsive menu settings.
+function genesis_sample_responsive_menu_settings() {
+
+	$settings = array(
+		'mainMenu'          => __( '', 'genesis-sample' ),
+		'menuIconClass'     => 'dashicons-before dashicons-menu',
+		'subMenu'           => __( '', 'genesis-sample' ),
+		'subMenuIconsClass' => 'dashicons-before dashicons-plus',
+		'menuClasses'       => array(
+			'combine' => array(
+				'.nav-primary',
+				'.nav-header',
+			),
+			'others'  => array(),
+		),
+	);
+
+	return $settings;
+
+}
+
+// Add HTML5 markup structure.
+add_theme_support( 'html5', array( 'caption', 'comment-form', 'comment-list', 'gallery', 'search-form' ) );
+
+// Add Accessibility support.
+add_theme_support( 'genesis-accessibility', array( '404-page', 'drop-down-menu', 'headings', 'rems', 'search-form', 'skip-links' ) );
+
+// Add viewport meta tag for mobile browsers.
+add_theme_support( 'genesis-responsive-viewport' );
+
+// Add support for custom header.
+add_theme_support( 'custom-header', array(
+	'width'           => 600,
+	'height'          => 160,
+	'header-selector' => '.site-title a',
+	'header-text'     => false,
+	'flex-height'     => true,
+) );
+
+// Add support for custom background.
+add_theme_support( 'custom-background' );
+
+// Add support for custom logo.
+add_theme_support( 'custom-logo', array(
+	'width'       => 600,
+	'height'      => 160,
+	'flex-width' => true,
+	'flex-height' => true,
+) );
+
+add_filter( 'genesis_seo_title', 'custom_header_inline_logo', 10, 3 );
+/**
+ * Add an image inline in the site title element for the logo
+ *
+ * @param string $title Current markup of title.
+ * @param string $inside Markup inside the title.
+ * @param string $wrap Wrapping element for the title.
+ *
+ * @author @_AlphaBlossom
+ * @author @_neilgee
+ * @author @_JiveDig
+ * @author @_srikat
+ */
+function custom_header_inline_logo( $title, $inside, $wrap ) {
+	// If the custom logo function and custom logo exist, set the logo image element inside the wrapping tags.
+	if ( function_exists( 'has_custom_logo' ) && has_custom_logo() ) {
+		$inside = sprintf( '<span class="screen-reader-text">%s</span>%s', esc_html( get_bloginfo( 'name' ) ), get_custom_logo() );
+	} else {
+		// If no custom logo, wrap around the site name.
+		$inside	= sprintf( '<a href="%s">%s</a>', trailingslashit( home_url() ), esc_html( get_bloginfo( 'name' ) ) );
+	}
+
+	// Build the title.
+	$title = genesis_markup( array(
+		'open'    => sprintf( "<{$wrap} %s>", genesis_attr( 'site-title' ) ),
+		'close'   => "</{$wrap}>",
+		'content' => $inside,
+		'context' => 'site-title',
+		'echo'    => false,
+		'params'  => array(
+			'wrap' => $wrap,
+		),
+	) );
+
+	return $title;
+}
+
+add_filter( 'genesis_attr_site-description', 'custom_add_site_description_class' );
+/**
+ * Add class for screen readers to site description.
+ * This will keep the site description markup but will not have any visual presence on the page
+ * This runs if there is a logo image set in the Customizer.
+ *
+ * @param array $attributes Current attributes.
+ *
+ * @author @_neilgee
+ * @author @_srikat
+ */
+function custom_add_site_description_class( $attributes ) {
+	if ( function_exists( 'has_custom_logo' ) && has_custom_logo() ) {
+		$attributes['class'] .= ' screen-reader-text';
+	}
+
+	return $attributes;
+}
+
+// Add support for after entry widget.
+add_theme_support( 'genesis-after-entry-widget-area' );
+
+// Add support for 4-column footer widgets.
+add_theme_support( 'genesis-footer-widgets', 4 );
+
+// Add Image Sizes.
+add_image_size( 'featured-image', 720, 400, TRUE );
+add_image_size( 'featured-team', 470, 230, array( 'center', 'top' ) );
+
+// Add custom image sizes to Beaver Builder
+add_filter( 'image_size_names_choose', 'insert_custom_image_sizes' );
+function insert_custom_image_sizes( $sizes ) {
+	global $_wp_additional_image_sizes;
+	if ( empty($_wp_additional_image_sizes) )
+		return $sizes;
+
+	foreach ( $_wp_additional_image_sizes as $id => $data ) {
+		if ( !isset($sizes[$id]) )
+			$sizes[$id] = ucfirst( str_replace( '-', ' ', $id ) );
+	}
+
+	return $sizes;
+}
+
+// Rename primary and secondary navigation menus.
+add_theme_support( 'genesis-menus', array( 'primary' => __( 'After Header Menu', 'genesis-sample' ), 'secondary' => __( 'Footer Menu', 'genesis-sample' ) ) );
+
+// Reposition the primary navigation menu.
+remove_action( 'genesis_after_header', 'genesis_do_nav' );
+add_action( 'genesis_header', 'genesis_do_nav' );
+
+// Reposition the secondary navigation menu.
+remove_action( 'genesis_after_header', 'genesis_do_subnav' );
+add_action( 'genesis_footer', 'genesis_do_subnav', 5 );
+
+// Reduce the secondary navigation menu to one level depth.
+add_filter( 'wp_nav_menu_args', 'genesis_sample_secondary_menu_args' );
+function genesis_sample_secondary_menu_args( $args ) {
+
+	if ( 'secondary' != $args['theme_location'] ) {
+		return $args;
+	}
+
+	$args['depth'] = 1;
+
+	return $args;
+
+}
+
+// Remove Entry Header from homepage
+add_action( 'genesis_before', 'gc_remove_entry_header' );
+function gc_remove_entry_header() {
+
+	if ( ! is_front_page() ) { return; }
+
+	//* Remove the entry header markup (requires HTML5 theme support)
+	remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_open', 5 );
+	remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_close', 15 );
+
+	//* Remove the entry title (requires HTML5 theme support)
+	remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
+
+	//* Remove the entry meta in the entry header (requires HTML5 theme support)
+	remove_action( 'genesis_entry_header', 'genesis_post_info', 12 );
+
+	//* Remove the post format image (requires HTML5 theme support)
+	remove_action( 'genesis_entry_header', 'genesis_do_post_format_image', 4 );
+
+
+}
+
+// Modify size of the Gravatar in the author box.
+add_filter( 'genesis_author_box_gravatar_size', 'genesis_sample_author_box_gravatar' );
+function genesis_sample_author_box_gravatar( $size ) {
+	return 90;
+}
+
+// Modify size of the Gravatar in the entry comments.
+add_filter( 'genesis_comment_list_args', 'genesis_sample_comments_gravatar' );
+function genesis_sample_comments_gravatar( $args ) {
+
+	$args['avatar_size'] = 60;
+
+	return $args;
+
+}
+remove_action( 'genesis_footer', 'genesis_do_footer' );
+
+// Add Site Terms widget area
+add_action( 'genesis_footer', 'gc_display_site_terms', 10 );
+function gc_display_site_terms() {
+	genesis_widget_area('site-terms',
+		array(
+           	 'before' => '<div class="site-terms"><div class="wrap">',
+		'after'   => '</div></div>',
+        	)
+	);
+}
+genesis_register_sidebar( array(
+	'id'          	=> 'site-terms',
+	'name'        	=> __( 'Site Terms', 'genesis-sample' ),
+	'description' 	=> __( 'This is the Site Terms footer area.', 'genesis-sample' ),
+) );
